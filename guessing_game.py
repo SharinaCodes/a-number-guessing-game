ceb89dd934f0fda07c89d6efe8c30871591362e3
Sharina V. Jones <135335430+SharinaCodes@users.noreply.github.com>
2025-04-22 21:22:53 -0400

    matching rubric

--- guessing_game.py
+++ guessing_game.py
@@ -11,7 +11,7 @@ from sys import exit
 
 # Global declarations
 
-player_guesses = []
+guess_totals = []
 game_attempts = 0
 best_attempt = 0
 
@@ -35,7 +35,7 @@ def check_guess(correct_number):
             player_guess = int(input("I'm thinking of a number between 1 and 100...\t"))
 
             if player_guess > 100 or player_guess < 0:
-                print("Number must be between 1 and 100 (inclusive)")
+                print("That number is invalid. Please try again.")
                 continue
             if player_guess > correct_number:
                 
@@ -49,30 +49,33 @@ def check_guess(correct_number):
             #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
                 print(f"Got it")
 
-            player_guesses.append(player_guess)
             guess_count += 1
         except ValueError:
+            print("That number is invalid. Please try again.")
             print("Please enter a whole number between 1 and 100 (inclusive)")
         except Exception as ex:
             print(f"Something went wrong: {ex}")
 
     return guess_count
 
-def show_stats(guess_count):
+def show_scores(guess_count):
     #   5. Display the following data to the player
     #     a. How many attempts it took them to get the correct number in this game
     print(f"Attempt #{game_attempts}")
     print(f"It took you {guess_count} guesses to get the correct number.")
     print(f"Your best attempt was {best_attempt} guesses")
     #     b. The mean of the saved attempts list
-    print(f"The average of your guesses is {round(mean(player_guesses))}.")
+
+def show_stats(guess_count):
+
+    print(f"The average of your guesses is {round(mean(guess_totals))}.")
 #     c. The median of the saved attempts list
-    print(f"The median of your guesses is {median(player_guesses)}")
+    print(f"The median of your guesses is {median(guess_totals)}")
 #     d. The mode of the saved attempts list
-    if len(multimode(player_guesses)) > 1:
+    if len(multimode(guess_totals)) > 1:
         print(f"There is no mode for you guesses")
     else:
-        print(f"The mode of your guesses is {multimode(player_guesses)}")
+        print(f"The mode of your guesses is {multimode(guess_totals)}")
     
 # Create the start_game function.
 def start_game():
@@ -89,19 +92,28 @@ def start_game():
 # Kick off the program by calling the start_game function.
 while True:
     game_attempts += 1
-    guess_count = start_game() 
+    guess_count = start_game()
+    guess_totals.append(guess_count)
+
     if game_attempts == 1:
         best_attempt = guess_count
     elif guess_count < best_attempt:
         best_attempt = guess_count
 
-    show_stats(guess_count)
+    show_scores(guess_count)
 
     #   6. Prompt the player to play again
     #     a. If they decide to play again, start the game loop over.
     #     b. If they decide to quit, show them a goodbye message.
-    play_again = input("Y for yes, N for no:\t")
+    play_again = input("Y for yes, anything else to exit:\t")
     if play_again.upper() == 'Y':
         continue
     else:
+        print("""
+-------------------------------------
+Final Stats!
+-------------------------------------
+              """)
+        show_stats(guess_count)
+        print("Thanks for playing!")
         exit()
